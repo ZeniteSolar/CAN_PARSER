0446c11a0bfa93dc522672255a1f9ced71b0b709
alejopm03 <perdomomilar@gmail.com>
2022-08-10 20:48:20 -0300

    add more tests

--- tests.c
+++ tests.c
@@ -6,7 +6,7 @@
 #include "can_ids.h"
 #include "can_parser.h"
 
-struct test_t
+struct test_mic_t
 {
     uint8_t signature; // Senders signature. Units:
     struct
@@ -19,7 +19,7 @@ struct test_t
     uint8_t d; // Motor Duty Cycle. Units: %
     uint8_t i; // Motor Soft Start. Units: %
 };
-struct test_t test;
+struct test_mic_t test_mic;
 
 static void parse_mic_state(can_msg_t *msg)
 {
@@ -30,12 +30,12 @@ static void parse_mic_motor(can_msg_t *msg)
 {
     CAN_DECLARE_MSG_OF_TYPE(can_mic19_motor_msg_t, mic_motor, msg);
 
-    test.signature = mic_motor->signature;
-    test.motor.motor_on = mic_motor->motor.motor_on;
-    test.motor.dms_on = mic_motor->motor.dms_on;
-    test.motor.reverse = mic_motor->motor.reverse;
-    test.d = mic_motor->d;
-    test.i = mic_motor->i;
+    test_mic.signature = mic_motor->signature;
+    test_mic.motor.motor_on = mic_motor->motor.motor_on;
+    test_mic.motor.dms_on = mic_motor->motor.dms_on;
+    test_mic.motor.reverse = mic_motor->motor.reverse;
+    test_mic.d = mic_motor->d;
+    test_mic.i = mic_motor->i;
 }
 
 static void parse_mswi_state(can_msg_t *msg)
@@ -43,9 +43,27 @@ static void parse_mswi_state(can_msg_t *msg)
     printf("mswi state");
 }
 
+struct test_mswi_t
+{
+    uint8_t signature; // Senders signature. Units:
+    struct
+    { // Motor state
+        uint8_t motor_on : 1;
+        uint8_t dms_on : 1;
+        uint8_t _unused : 6;
+    } motor;
+    uint8_t d; // Motor Duty Cycle. Units: %
+    uint8_t i; // Motor Soft Start. Units: %
+} test_mswi;
+
 static void parse_mswi_motor(can_msg_t *msg)
 {
-    printf("mswi motor");
+    CAN_DECLARE_MSG_OF_TYPE(can_mswi19_motor_msg_t, mswi_motor, msg);
+    test_mswi.signature = mswi_motor->signature;
+    test_mswi.d = mswi_motor->d;
+    test_mswi.i = mswi_motor->i;
+    test_mswi.motor.dms_on = mswi_motor->motor.dms_on;
+    test_mswi.motor.motor_on = mswi_motor->motor.motor_on;
 }
 
 CAN_REGISTER_TOPICS(mic,
@@ -60,41 +78,72 @@ CAN_REGISTER_MODULES({CAN_SIGNATURE_MIC19, &CAN_TOPICS_NAME(mic)},
 
 CAN_REGISTER_PARSER(mam);
 
-int main()
+int main(void)
 {
 
     /*
-     *   Test case
+     *   Test case MIC -> MAM msg
      */
     can_msg_t msg;
+    for (int i = 0; i <= 255; i++)
+    {
+        msg.raw[CAN_MSG_GENERIC_STATE_SIGNATURE_BYTE] = CAN_SIGNATURE_MIC19;
+        msg.raw[CAN_MSG_MIC19_MOTOR_D_BYTE] = i;
+        msg.raw[CAN_MSG_MIC19_MOTOR_I_BYTE] = 255 - i;
 
-    msg.raw[CAN_MSG_GENERIC_STATE_SIGNATURE_BYTE] = CAN_SIGNATURE_MIC19;
-    msg.raw[CAN_MSG_MIC19_MOTOR_D_BYTE] = 128;
-    msg.raw[CAN_MSG_MIC19_MOTOR_I_BYTE] = 255;
+        msg.raw[CAN_MSG_MIC19_MOTOR_MOTOR_BYTE] =
+            (((i % 2) == 0) << CAN_MSG_MIC19_MOTOR_MOTOR_MOTOR_ON_BIT);
 
-    msg.raw[CAN_MSG_MIC19_MOTOR_MOTOR_BYTE] =
-        ((1) << CAN_MSG_MIC19_MOTOR_MOTOR_MOTOR_ON_BIT);
+        msg.raw[CAN_MSG_MIC19_MOTOR_MOTOR_BYTE] |=
+            (((i % 2) == 0) << CAN_MSG_MIC19_MOTOR_MOTOR_DMS_ON_BIT);
 
-    msg.raw[CAN_MSG_MIC19_MOTOR_MOTOR_BYTE] |=
-        ((0) << CAN_MSG_MIC19_MOTOR_MOTOR_DMS_ON_BIT);
+        msg.raw[CAN_MSG_MIC19_MOTOR_MOTOR_BYTE] |=
+            (((i % 2) != 0) << CAN_MSG_MIC19_MOTOR_MOTOR_REVERSE_BIT);
+        msg.id = CAN_MSG_MIC19_MOTOR_ID;
 
-    msg.raw[CAN_MSG_MIC19_MOTOR_MOTOR_BYTE] |=
-        ((1) << CAN_MSG_MIC19_MOTOR_MOTOR_REVERSE_BIT);
-    msg.id = CAN_MSG_MIC19_MOTOR_ID;
+        printf("parsing %d ...\n", msg.signature);
 
-    can_parse_mam(&msg);
+        can_parse_mam(&msg);
 
-    printf("test.signature: %d\n", test.signature);
-    printf("test.d: %d\n", test.d);
-    printf("test.i: %d\n", test.i);
-    printf("test.motor.motor_on: %d\n", test.motor.motor_on);
-    printf("test.motor.dms_on: %d\n", test.motor.dms_on);
-    printf("test.motor.reverse: %d\n", test.motor.reverse);
+        printf("test_mic.signature: %d\n", test_mic.signature);
+        printf("test_mic.d: %d\n", test_mic.d);
+        printf("test_mic.i: %d\n", test_mic.i);
+        printf("test_mic.motor.motor_on: %d\n", test_mic.motor.motor_on);
+        printf("test_mic.motor.dms_on: %d\n", test_mic.motor.dms_on);
+        printf("test_mic.motor.reverse: %d\n", test_mic.motor.reverse);
 
-    assert(CAN_SIGNATURE_MIC19 == test.signature);
-    assert(129 == test.d);
-    assert(255 == test.i);
-    assert(1 == test.motor.motor_on);
-    assert(0 == test.motor.dms_on);
-    assert(1 == test.motor.reverse);
+        assert(CAN_SIGNATURE_MIC19 == test_mic.signature);
+        assert(i == test_mic.d);
+        assert(255 - i == test_mic.i);
+        assert(((i % 2) == 0) == test_mic.motor.motor_on);
+        assert(((i % 2) == 0) == test_mic.motor.dms_on);
+        assert(((i % 2) != 0) == test_mic.motor.reverse);
+
+        msg.id = CAN_MSG_MSWI19_MOTOR_ID;
+
+        msg.raw[CAN_MSG_GENERIC_STATE_SIGNATURE_BYTE] = CAN_SIGNATURE_MSWI19;
+
+        msg.raw[CAN_MSG_MSWI19_MOTOR_D_BYTE] = 255 - i;
+
+        msg.raw[CAN_MSG_MSWI19_MOTOR_I_BYTE] = i;
+        msg.raw[CAN_MSG_MSWI19_MOTOR_MOTOR_BYTE] = 0;
+        msg.raw[CAN_MSG_MSWI19_MOTOR_MOTOR_BYTE] |= (((i % 2) == 0) << CAN_MSG_MSWI19_MOTOR_MOTOR_MOTOR_ON_BIT);
+        msg.raw[CAN_MSG_MSWI19_MOTOR_MOTOR_BYTE] |= (((i % 2) != 0) << CAN_MSG_MSWI19_MOTOR_MOTOR_DMS_ON_BIT);
+
+        printf("parsing %d ...\n", msg.signature);
+
+        can_parse_mam(&msg);
+
+        printf("test_mswi.signature: %d\n", test_mswi.signature);
+        printf("test_mswi.d: %d\n", test_mswi.d);
+        printf("test_mswi.i: %d\n", test_mswi.i);
+        printf("test_mswi.motor.motor_on: %d\n", test_mswi.motor.motor_on);
+        printf("test_mswi.motor.dms_on: %d\n", test_mswi.motor.dms_on);
+
+        assert(CAN_SIGNATURE_MSWI19 == test_mswi.signature);
+        assert(i == test_mswi.i);
+        assert(255 - i == test_mswi.d);
+        assert(((i % 2) == 0) == test_mswi.motor.motor_on);
+        assert(((i % 2) != 0) == test_mswi.motor.dms_on);
+    }
 }
